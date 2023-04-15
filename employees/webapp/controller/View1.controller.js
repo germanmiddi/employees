bd65fa5fbbcea013c5efe4dd5dc25fd2bffe5430
BTP Server <german.middi@gmail.com>
2023-04-15 20:05:58 +0000

    Bloque IV 2.3

--- employees/webapp/controller/View1.controller.js
+++ employees/webapp/controller/View1.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -10,6 +11,18 @@ sap.ui.define([
         return Controller.extend("employees.controller.View1", {
             onInit: function () {
 
+            },
+            onValidate: function(){
+                let inputEmployee = this.byId("inputEmployee")
+                let valueEmployee = inputEmployee.getValue()
+
+                if(valueEmployee.length === 6){
+                    this.byId("labelCountry").setVisible(true)
+                    this.byId("slCountry").setVisible(true)
+                }else{
+                    this.byId("labelCountry").setVisible(false)
+                    this.byId("slCountry").setVisible(false)
+                }
             }
         });
     });
